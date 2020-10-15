759d8a44ca181b9b1ebf6808fa0413b54fb158bf
Carlos Roa <croa@pros.com>
2020-10-15 16:04:01 -0500

    PPSS-39595 Release application browser 4.0.1
    Addressing code review comments

--- changeWinMaker.js
+++ changeWinMaker.js
@@ -1,10 +1,5 @@
 /*
- * Copyright (c) 2020 by PROS, Inc.  All Rights Reserved.
- * This software is the confidential and proprietary information of
- * PROS, Inc. ("Confidential Information").
- * You may not disclose such Confidential Information, and may only
- * use such Confidential Information in accordance with the terms of
- * the license agreement you entered into with PROS.
+ * Copyright (c) 2020 by PROS, Inc.
  */
 
 var fs = require('fs');
